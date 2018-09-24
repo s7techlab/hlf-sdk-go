c23aba2b4f7800a4c3019bb94de0cf2251c26252
Dmitry Zhvakin <dawner@yandex.ru>
2018-09-24 05:37:29 -0700

    - fix grpc func logging

--- peer/grpc.go
+++ peer/grpc.go
@@ -16,17 +16,25 @@ func NewGRPCFromConfig(c config.PeerConfig, log *zap.Logger) (*grpc.ClientConn, 
 	var grpcOptions []grpc.DialOption
 
 	if c.Tls.Enabled {
+		l.Debug(`Using TLS credentials`)
 		if ts, err := credentials.NewClientTLSFromFile(c.Tls.CertPath, ``); err != nil {
+			l.Error(`Failed to read TLS credentials`, zap.Error(err))
 			return nil, errors.Wrap(err, `failed to read tls credentials`)
 		} else {
+			l.Debug(`Read TLS credentials`, zap.Reflect(`cred`, ts.Info()))
 			grpcOptions = append(grpcOptions, grpc.WithTransportCredentials(ts))
 		}
 	} else {
+		l.Debug(`TLS is not used`)
 		grpcOptions = append(grpcOptions, grpc.WithInsecure())
 	}
 
 	// Set KeepAlive parameters if presented
 	if c.GRPC.KeepAlive != nil {
+		l.Debug(`Using KeepAlive params`,
+			zap.Duration(`time`, time.Duration(c.GRPC.KeepAlive.Time)*time.Second),
+			zap.Duration(`timeout`, time.Duration(c.GRPC.KeepAlive.Timeout)*time.Second),
+		)
 		grpcOptions = append(grpcOptions, grpc.WithKeepaliveParams(keepalive.ClientParameters{
 			Time:    time.Duration(c.GRPC.KeepAlive.Time) * time.Second,
 			Timeout: time.Duration(c.GRPC.KeepAlive.Timeout) * time.Second,
