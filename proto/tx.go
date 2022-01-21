480b81095ef0fd73435fe1ce444107369cdfb410
Andrey Bogatyr285 Bogatyrev <bogatyr285@gmail.com>
2022-01-21 14:04:04 +0300

    tx actions

--- proto/tx.go
+++ proto/tx.go
@@ -1,20 +1,28 @@
 package proto
 
 import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric-protos-go/msp"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/hyperledger/fabric/protoutil"
+	"github.com/pkg/errors"
 )
 
 type (
 	Transaction struct {
-		Actions TransactionsActions
+		Actions         TransactionsActions
+		CreatorIdentity msp.SerializedIdentity
+		Signature       []byte
 	}
 
 	Transactions []*Transaction
 )
 
-func ParseEndorserTx(envelopePayloadData []byte) (*Transaction, error) {
-	tx, err := protoutil.UnmarshalTransaction(envelopePayloadData)
+func ParseEndorserTx(envelopePayloadBytes []byte) (*Transaction, error) {
+	tx, err := protoutil.UnmarshalTransaction(envelopePayloadBytes)
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +33,30 @@ func ParseEndorserTx(envelopePayloadData []byte) (*Transaction, error) {
 		return nil, err
 	}
 
+	payload, err := protoutil.UnmarshalPayload(envelopePayloadBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, `failed to get payload from envelope`)
+	}
+
+	sigHeader, err := protoutil.UnmarshalSignatureHeader(payload.Header.SignatureHeader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get signature header: %w", err)
+	}
+
+	parsedTx.CreatorIdentity, err = ParseTransactionCreator(sigHeader)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction creator: %w", err)
+	}
+
 	return parsedTx, err
+}
+
+func ParseTransactionCreator(sigHeader *common.SignatureHeader) (msp.SerializedIdentity, error) {
+	creatorIdentity := &msp.SerializedIdentity{}
+	if err := proto.Unmarshal(sigHeader.Creator, creatorIdentity); err != nil {
+		return msp.SerializedIdentity{}, fmt.Errorf("failed to get creator identity: %w", err)
+	}
+	return *creatorIdentity, nil
 }
 
 func (t *Transaction) Events() []*peer.ChaincodeEvent {
--- proto/tx_action.go
+++ proto/tx_action.go
@@ -3,13 +3,20 @@ package proto
 import (
 	"fmt"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/ledger/rwset"
+	"github.com/hyperledger/fabric-protos-go/ledger/rwset/kvrwset"
+	"github.com/hyperledger/fabric-protos-go/msp"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/hyperledger/fabric/protoutil"
 )
 
 type (
 	TransactionAction struct {
-		Event *peer.ChaincodeEvent
+		Event                   *peer.ChaincodeEvent
+		Endorsers               []*msp.SerializedIdentity
+		ReadWriteSets           []*kvrwset.KVRWSet
+		ChaincodeInvocationSpec *peer.ChaincodeInvocationSpec
 	}
 
 	TransactionsActions []*TransactionAction
@@ -30,7 +37,42 @@ func ParseTxActions(txActions []*peer.TransactionAction) ([]*TransactionAction, 
 }
 
 func ParseTxAction(txAction *peer.TransactionAction) (*TransactionAction, error) {
+	ccAction, err := ParseChaincodeAction(txAction)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction chaincode action: %w", err)
+	}
 
+	ccEvent, err := ParseTransactionActionEvents(ccAction)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction events: %w", err)
+	}
+
+	endorsers, err := ParseTransactionActionEndorsers(txAction)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction endorsers: %w", err)
+	}
+
+	rwSets, err := ParseTransactionActionReadWriteSet(ccAction)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction read/write sets: %w", err)
+	}
+
+	chaincodeInvocationSpec, err := ParseTransactionActionChaincode(txAction)
+	if err != nil {
+		return nil, fmt.Errorf("parse transaction chaincode invocation spec: %w", err)
+	}
+
+	parsedTxAction := &TransactionAction{
+		Event:                   ccEvent,
+		Endorsers:               endorsers,
+		ReadWriteSets:           rwSets,
+		ChaincodeInvocationSpec: chaincodeInvocationSpec,
+	}
+
+	return parsedTxAction, nil
+}
+
+func ParseChaincodeAction(txAction *peer.TransactionAction) (*peer.ChaincodeAction, error) {
 	ccActionPayload, err := protoutil.UnmarshalChaincodeActionPayload(txAction.Payload)
 	if err != nil {
 		return nil, fmt.Errorf(`chaincode action payload: %w`, err)
@@ -47,14 +89,69 @@ func ParseTxAction(txAction *peer.TransactionAction) (*TransactionAction, error)
 		return nil, fmt.Errorf(`chaincode action from proposal response: %w`, err)
 	}
 
+	return chaincodeAction, nil
+}
+
+func ParseTransactionActionEvents(chaincodeAction *peer.ChaincodeAction) (*peer.ChaincodeEvent, error) {
 	ccEvent, err := protoutil.UnmarshalChaincodeEvents(chaincodeAction.Events)
 	if err != nil {
 		return nil, fmt.Errorf(`event from chaincode action: %w`, err)
 	}
+	return ccEvent, nil
+}
 
-	parsedTxAction := &TransactionAction{
-		Event: ccEvent,
+func ParseTransactionActionEndorsers(txAction *peer.TransactionAction) ([]*msp.SerializedIdentity, error) {
+	ccActionPayload, err := protoutil.UnmarshalChaincodeActionPayload(txAction.Payload)
+	if err != nil {
+		return nil, fmt.Errorf(`chaincode action payload: %w`, err)
 	}
 
-	return parsedTxAction, nil
+	endorsers := make([]*msp.SerializedIdentity, 0)
+	for _, en := range ccActionPayload.Action.Endorsements {
+		endorser := &msp.SerializedIdentity{}
+
+		if err := proto.Unmarshal(en.Endorser, endorser); err != nil {
+			return nil, fmt.Errorf("failed to get endorser: %w", err)
+		}
+
+		endorsers = append(endorsers, endorser)
+	}
+
+	return endorsers, nil
+}
+
+func ParseTransactionActionReadWriteSet(chaincodeAction *peer.ChaincodeAction) ([]*kvrwset.KVRWSet, error) {
+	txReadWriteSet := &rwset.TxReadWriteSet{}
+	if err := proto.Unmarshal(chaincodeAction.Results, txReadWriteSet); err != nil {
+		return nil, fmt.Errorf("failed to get txReadWriteSet: %w", err)
+	}
+
+	kvReadWriteSets := make([]*kvrwset.KVRWSet, 0)
+	for _, rw := range txReadWriteSet.NsRwset {
+		kvReadWriteSet := &kvrwset.KVRWSet{}
+		if err := proto.Unmarshal(rw.Rwset, kvReadWriteSet); err != nil {
+			return nil, fmt.Errorf("failed to get kvReadWriteSet: %w", err)
+		}
+		kvReadWriteSets = append(kvReadWriteSets, kvReadWriteSet)
+	}
+
+	return kvReadWriteSets, nil
+}
+
+func ParseTransactionActionChaincode(txAction *peer.TransactionAction) (*peer.ChaincodeInvocationSpec, error) {
+	actionPayload, err := protoutil.UnmarshalChaincodeActionPayload(txAction.Payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get chaincode action from action payload: %w", err)
+	}
+
+	chaincodeProposalPayload, err := protoutil.UnmarshalChaincodeProposalPayload(actionPayload.ChaincodeProposalPayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get chaincode proposal from action payload: %w", err)
+	}
+
+	chaincodeInvocationSpec := &peer.ChaincodeInvocationSpec{}
+	if err = proto.Unmarshal(chaincodeProposalPayload.Input, chaincodeInvocationSpec); err != nil {
+		return nil, fmt.Errorf("failed to get chaincode invocation spec from action payload: %w", err)
+	}
+	return chaincodeInvocationSpec, nil
 }
