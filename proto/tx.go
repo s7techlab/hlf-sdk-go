ecffb41333fbb8723fc3063f07eccf8e06397690
Andrey Bogatyr285 Bogatyrev <bogatyr285@gmail.com>
2022-01-31 16:15:30 +0300

    rm comments

--- proto/tx.go
+++ proto/tx.go
@@ -2,7 +2,6 @@ package proto
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/hyperledger/fabric-protos-go/common"
 	"github.com/hyperledger/fabric-protos-go/msp"
@@ -27,17 +26,7 @@ func ParseTransaction(payload *common.Payload, transactionType common.HeaderType
 
 	si, err := protoutil.UnmarshalSerializedIdentity(sigHeader.Creator)
 	if err != nil {
-		// in some transactions we get some unknown proto message with chaincodes(!?), dont know how to deal with it now
-		// ---- example
-		// �
-		// �
-		// _lifecycle�
-		// "ApproveChaincodeDefinitionForMyOrg
-		// ^basic1.0JNL
-		// Jbasic_1.0:770d76a4369f9121d4945c6782dd42c4db7c130c3c7b77b73d9894414b5a3da9
-		log.Println("[hlf.ptoto.tx-parser] got error, skipping it. transaction creator is not 'msp.SerializedIdentity': ", string(sigHeader.Creator))
-		err = nil
-		//return nil, fmt.Errorf("parse transaction creator: %w", err)
+		return nil, fmt.Errorf("parse transaction creator: %w", err)
 	}
 
 	var actions []*TransactionAction
