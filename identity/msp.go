06980bc15c356507de81b5329cdfeec468a8866e
vitiko <vitiko@mail.ru>
2022-03-22 19:08:01 +0300

    msp loader refactoring

--- identity/msp.go
+++ identity/msp.go
@@ -1,108 +1,179 @@
 package identity
 
 import (
-	"errors"
+	"crypto/x509"
+	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/s7techlab/hlf-sdk-go/api"
 )
 
-var (
-	ErrSignCertsDirIsEmpty = errors.New(`"signcerts"" folder is empty`)
-)
-
 // MSP - contains all parsed identities from msp folder
 // Should be used instead of single `api.Identity` which contains ONLY msp identity
-type MSP struct {
-	// identity from 'signcerts'
-	MSP api.Identity
-	// identities from 'admincerts'
-	Admins []api.Identity
-	// identities from 'users'
-	Users []api.Identity
+
+type (
+	MSPContent struct {
+		id string
+		// identity from 'signcerts'
+		signer api.Identity
+		// identities from 'admincerts'
+		admins []api.Identity
+		// identities from 'users'
+		users []api.Identity
+
+		caCerts           []*x509.Certificate
+		intermediateCerts []*x509.Certificate
+	}
+
+	MSP interface {
+		GetMSPIdentifier() string
+		Signer() api.Identity
+		Admins() []api.Identity
+		Users() []api.Identity
+		CACerts() []*x509.Certificate
+		IntermediateCerts() []*x509.Certificate
+
+		AdminOrSigner() api.Identity
+	}
+
+	MspOpts struct {
+		mspPath        string
+		admincertsPath string
+		signcertsPath  string
+		keystorePath   string
+
+		loadUsers bool
+		userPaths []string
+
+		cacertsPath           string
+		intermediatecertsPath string
+
+		loadCertChain     bool
+		validateCertChain bool
+	}
+
+	MspOpt func(opts *MspOpts)
+)
+
+func WithMSPPath(path string) MspOpt {
+	return func(opts *MspOpts) {
+		opts.mspPath = path
+	}
 }
 
-// CollectionFromMSPPath  parse all certificates(msp,admins,users) from MSP folder.
-// Came to replace legacy `util.LoadKeyPairFromMSP` method
-func CollectionFromMSPPath(mspID string, mspPath string) (*MSP, error) {
-
-	mspIdentities := &MSP{
-		Admins: make([]api.Identity, 0),
-		MSP:    nil,
-		Users:  make([]api.Identity, 0),
+func WithCertChain() MspOpt {
+	return func(opts *MspOpts) {
+		opts.loadCertChain = true
 	}
-
-	// admin certs
-	adminDir := filepath.Join(mspPath, MSPAdmincertsPath)
-	_, err := os.Stat(adminDir)
-	if !os.IsNotExist(err) {
-		adminCerts, err := ReadAllFilesFromDir(adminDir)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, adminCertBytes := range adminCerts {
-			cert, key, err := LoadKeypairByCert(mspPath, adminCertBytes)
-			if err != nil {
-				return nil, err
-			}
-
-			idnt, _ := New(mspID, cert, key)
-			mspIdentities.Admins = append(mspIdentities.Admins, idnt)
-		}
-	}
-
-	// user certs
-	userDir := filepath.Join(mspPath, MSPUserscertsPath)
-	_, err = os.Stat(userDir)
-	if !os.IsNotExist(err) {
-		userCerts, err := ReadAllFilesFromDir(userDir)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, userCertBytes := range userCerts {
-			cert, key, err := LoadKeypairByCert(mspPath, userCertBytes)
-			if err != nil {
-				return nil, err
-			}
-
-			idnt, _ := New(mspID, cert, key)
-			mspIdentities.Users = append(mspIdentities.Users, idnt)
-		}
-	}
-
-	// signcert
-	signCertsDir := filepath.Join(mspPath, MSPSigncertsPath)
-	signCerts, err := ReadAllFilesFromDir(signCertsDir)
-	if err != nil {
-		return nil, err
-	}
-	if len(signCerts) == 0 {
-		return nil, ErrSignCertsDirIsEmpty
-	}
-
-	cert, key, err := LoadKeypairByCert(mspPath, signCerts[0])
-	if err != nil {
-		return nil, err
-	}
-
-	idnt, _ := New(mspID, cert, key)
-	mspIdentities.MSP = idnt
-
-	return mspIdentities, nil
 }
 
-// PreferAdminIfExists - returns admin identity if exists, in another case return msp.
+func applyDefaultMSPPaths(mspOpts *MspOpts) {
+	if mspOpts.admincertsPath == `` {
+		mspOpts.admincertsPath = AdmincertsPath(mspOpts.mspPath)
+	}
+
+	if mspOpts.signcertsPath == `` {
+		mspOpts.signcertsPath = SigncertsPath(mspOpts.mspPath)
+	}
+
+	if mspOpts.keystorePath == `` {
+		mspOpts.keystorePath = KeystorePath(mspOpts.mspPath)
+	}
+
+	if len(mspOpts.userPaths) == 0 && mspOpts.loadUsers {
+		mspOpts.userPaths = []string{UsercertsPath(mspOpts.mspPath)}
+	}
+
+	if mspOpts.cacertsPath == `` {
+		mspOpts.cacertsPath = CacertsPath(mspOpts.mspPath)
+	}
+
+	if mspOpts.intermediatecertsPath == `` {
+		mspOpts.intermediatecertsPath = IntermediatecertsPath(mspOpts.mspPath)
+	}
+}
+
+func NewMSP(mspID string, opts ...MspOpt) (*MSPContent, error) {
+	var err error
+	mspOpts := &MspOpts{}
+	for _, opt := range opts {
+		opt(mspOpts)
+	}
+
+	applyDefaultMSPPaths(mspOpts)
+
+	mspContent := &MSPContent{}
+
+	if mspOpts.admincertsPath != `` {
+		mspContent.admins, err = ListFromPath(mspID, mspOpts.admincertsPath, mspOpts.keystorePath)
+		if !os.IsNotExist(err) {
+			return nil, fmt.Errorf(`read admin identity from=%s: %w`, mspOpts.admincertsPath, err)
+		}
+	}
+
+	for _, userPath := range mspOpts.userPaths {
+		users, err := ListFromPath(mspID, userPath, mspOpts.keystorePath)
+		// usePaths set explicit, so if dir is not exists - error occurred
+		if err != nil {
+			return nil, fmt.Errorf(`read users identity from=%s: %w`, userPath, err)
+		}
+
+		mspContent.users = append(mspContent.users, users...)
+	}
+
+	if mspOpts.signcertsPath != `` {
+		mspContent.signer, err = FirstFromPath(mspID, mspOpts.signcertsPath, mspOpts.keystorePath)
+	}
+
+	if mspOpts.loadCertChain {
+		mspContent.caCerts, err = CertificatesFromPath(mspOpts.cacertsPath)
+		if err != nil {
+			return nil, fmt.Errorf(`read cacerts from=%s: %w`, mspOpts.cacertsPath, err)
+		}
+
+		mspContent.intermediateCerts, err = CertificatesFromPath(mspOpts.intermediatecertsPath)
+		if err != nil {
+			return nil, fmt.Errorf(`read intermediatecerts from=%s: %w`, mspOpts.cacertsPath, err)
+		}
+	}
+
+	if mspOpts.validateCertChain {
+		// todo: validate
+	}
+	return mspContent, nil
+}
+
+func (m *MSPContent) GetMSPIdentifier() string {
+	return m.id
+}
+
+func (m *MSPContent) Signer() api.Identity {
+	return m.signer
+}
+
+func (m *MSPContent) Admins() []api.Identity {
+	return m.admins
+}
+
+func (m *MSPContent) Users() []api.Identity {
+	return m.users
+}
+
+func (m *MSPContent) CACerts() []*x509.Certificate {
+	return m.caCerts
+}
+
+func (m *MSPContent) IntermediateCerts() []*x509.Certificate {
+	return m.intermediateCerts
+}
+
+// AdminOrSigner - returns admin identity if exists, in another case return msp.
 // installation, fetching  cc list should happen from admin identity
 // if there is admin identity, use it. in another case - try with msp identity
-func (m *MSP) PreferAdminIfExists() api.Identity {
-	var identity api.Identity
-	if len(m.Admins) != 0 {
-		identity = m.Admins[0]
-	} else {
-		identity = m.MSP
+func (m *MSPContent) AdminOrSigner() api.Identity {
+	if len(m.admins) != 0 {
+		return m.admins[0]
 	}
-	return identity
+
+	return m.signer
 }
