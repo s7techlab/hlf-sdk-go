88b834d84cf195eb89ccf57df0609fc5e0b08cfe
vitiko <vitiko@mail.ru>
2022-03-27 16:01:53 +0300

    tests

--- identity/msp_test.go
+++ identity/msp_test.go
@@ -1,12 +1,13 @@
 package identity_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"testing"
-
 	"github.com/s7techlab/hlf-sdk-go/identity"
+	"github.com/s7techlab/hlf-sdk-go/identity/testdata/Org1MSP"
 )
 
 func TestIdentity(t *testing.T) {
@@ -27,8 +28,29 @@ var _ = Describe(`Cert`, func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(msp).NotTo(BeNil())
 
-			Expect(msp.Signer()).NotTo(BeNil())
-		})
+			Expect(msp.Admins()).To(HaveLen(1))
+			Expect(msp.Admins()[0].GetPEM()).To(Equal(Org1MSP.AdminCert))
 
+			Expect(msp.Signer()).NotTo(BeNil())
+			Expect(msp.Signer().GetPEM()).To(Equal(Org1MSP.SignCert))
+
+			Expect(msp.CACerts()).To(HaveLen(1))
+			Expect(identity.PEMEncode(msp.CACerts()[0].Raw)).To(Equal(Org1MSP.CACert))
+
+			Expect(msp.IntermediateCerts()).To(HaveLen(0))
+
+			ouConfig := msp.OUConfig()
+			Expect(ouConfig).NotTo(BeNil())
+
+			// configured in testdata/Org1MSP/config.yaml
+			Expect(ouConfig.UnitIdentifiers).To(HaveLen(0))
+			Expect(ouConfig.NodeOUs).NotTo(BeNil())
+
+			Expect(ouConfig.NodeOUs.ClientOuIdentifier.Certificate).To(Equal(Org1MSP.CACert))
+			Expect(ouConfig.NodeOUs.PeerOuIdentifier.Certificate).To(Equal(Org1MSP.CACert))
+			Expect(ouConfig.NodeOUs.AdminOuIdentifier.Certificate).To(Equal(Org1MSP.CACert))
+			Expect(ouConfig.NodeOUs.OrdererOuIdentifier.Certificate).To(Equal(Org1MSP.CACert))
+
+		})
 	})
 })
---
+++ identity/testdata/Org1MSP/testdata.go
@@ -0,0 +1,16 @@
+package Org1MSP
+
+import (
+	_ "embed"
+)
+
+var (
+	//go:embed admincerts/cert.pem
+	AdminCert []byte
+
+	//go:embed signcerts/cert.pem
+	SignCert []byte
+
+	//go:embed cacerts/localhost-7054-ca-org1.pem
+	CACert []byte
+)
