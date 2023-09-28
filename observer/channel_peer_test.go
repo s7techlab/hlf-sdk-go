3a3012a7124e4ca39c972ef16b3e72074b41b35d
Nikita Neznaemov <n.neznaemov@atomyze.ru>
2023-09-28 11:19:07 +0300

    disable-observer-parsing

---
+++ observer/channel_peer_test.go
@@ -0,0 +1,60 @@
+package observer_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/s7techlab/hlf-sdk-go/observer"
+	testdata "github.com/s7techlab/hlf-sdk-go/testdata/blocks"
+)
+
+var _ = Describe("Channel peer", func() {
+	var (
+		channelPeerFetcherMock observer.PeerChannelsFetcher
+	)
+	BeforeEach(func() {
+		channelPeerFetcherMock = observer.NewChannelPeerFetcherMock(testdata.ChannelsHeights)
+	})
+
+	It("default channel peer, no channel matcher", func() {
+		channelPeer, err := observer.NewChannelPeer(channelPeerFetcherMock)
+		Expect(err).To(BeNil())
+
+		channelPeer.Observe(ctx)
+		time.Sleep(time.Millisecond * 100)
+
+		channelsMap := channelPeer.Channels()
+
+		sampleChannelInfo, exist := channelsMap[testdata.SampleChannel]
+		Expect(exist).To(BeTrue())
+		Expect(sampleChannelInfo.Channel).To(Equal(testdata.SampleChannel))
+		Expect(sampleChannelInfo.Height).To(Equal(testdata.SampleChannelHeight))
+
+		fabcarChannelInfo, exist := channelsMap[testdata.FabcarChannel]
+		Expect(exist).To(BeTrue())
+		Expect(fabcarChannelInfo.Channel).To(Equal(testdata.FabcarChannel))
+		Expect(fabcarChannelInfo.Height).To(Equal(testdata.FabcarChannelHeight))
+	})
+
+	It("default channel peer, with channel matcher, exclude Fabcar", func() {
+		channelPeer, err := observer.NewChannelPeer(channelPeerFetcherMock,
+			observer.WithChannels([]observer.ChannelToMatch{{MatchPattern: testdata.SampleChannel}}))
+		Expect(err).To(BeNil())
+
+		channelPeer.Observe(ctx)
+		time.Sleep(time.Millisecond * 100)
+
+		channelsMap := channelPeer.Channels()
+
+		sampleChannelInfo, exist := channelsMap[testdata.SampleChannel]
+		Expect(exist).To(BeTrue())
+		Expect(sampleChannelInfo.Channel).To(Equal(testdata.SampleChannel))
+		Expect(sampleChannelInfo.Height).To(Equal(testdata.SampleChannelHeight))
+
+		fabcarChannelInfo, exist := channelsMap[testdata.FabcarChannel]
+		Expect(exist).To(BeFalse())
+		Expect(fabcarChannelInfo).To(BeNil())
+	})
+})
---
+++ observer/transform/kvread.go
@@ -0,0 +1,107 @@
+package transform
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/ledger/rwset/kvrwset"
+
+	"github.com/s7techlab/hlf-sdk-go/util"
+)
+
+type (
+	KVReadTransformer interface {
+		Transform(*kvrwset.KVRead) error
+	}
+
+	KVReadMatch  func(*kvrwset.KVRead) bool
+	KVReadMutate func(*kvrwset.KVRead) error
+
+	KVRead struct {
+		match    KVReadMatch
+		mutators []KVReadMutate
+	}
+)
+
+func NewKVRead(match KVReadMatch, mutators ...KVReadMutate) *KVRead {
+	return &KVRead{
+		match:    match,
+		mutators: mutators,
+	}
+}
+
+func (kvread *KVRead) Transform(w *kvrwset.KVRead) error {
+	if !kvread.match(w) {
+		return nil
+	}
+	for _, mutate := range kvread.mutators {
+		if err := mutate(w); err != nil {
+			return fmt.Errorf(`kv read mutate: %w`, err)
+		}
+	}
+	return nil
+}
+
+func KVReadMatchKeyPrefix(prefixes ...string) KVReadMatch {
+	return func(read *kvrwset.KVRead) bool {
+		keyPrefix, _ := util.SplitCompositeKey(read.Key)
+		for _, prefix := range prefixes {
+			if keyPrefix == prefix {
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+func KVReadProtoWithKeyPrefix(prefix string, target proto.Message) *KVRead {
+	return NewKVRead(
+		KVReadMatchKeyPrefix(prefix),
+	)
+}
+
+func KVReadMutatorWithKeyPrefix(prefix string, mutator KVReadMutate) *KVRead {
+	return NewKVRead(
+		KVReadMatchKeyPrefix(prefix),
+		mutator,
+	)
+}
+
+func KVReadKeyObjectTypeReplaceByMap(mapping map[string]string, additionalMutators ...KVReadMutate) *KVRead {
+	return NewKVRead(
+		KVReadMatchKeyPrefix(MappingPrefixes(mapping)...),
+		append([]KVReadMutate{KVReadKeyReplacer(mapping)}, additionalMutators...)...,
+	)
+}
+
+func KVReadKeyReplacer(mapping map[string]string) KVReadMutate {
+	return func(read *kvrwset.KVRead) error {
+		prefix, attributes := util.SplitCompositeKey(read.Key)
+		if mappedPrefix, ok := mapping[prefix]; ok {
+			mappedKey, err := util.CreateCompositeKey(mappedPrefix, attributes)
+			if err != nil {
+				return fmt.Errorf(`create mapped composite key: %w`, err)
+			}
+			read.Key = mappedKey
+		}
+		return nil
+	}
+}
+
+func KVReadKeyReplace(mapping map[string]string, additionalMutators ...KVReadMutate) *KVRead {
+	return NewKVRead(KVReadMatchKey(MappingPrefixes(mapping)...), additionalMutators...)
+}
+
+func KVReadMatchKey(contents ...string) KVReadMatch {
+	return func(read *kvrwset.KVRead) bool {
+		for _, content := range contents {
+			if strings.Contains(read.Key, content) {
+				return true
+			}
+		}
+
+		return false
+	}
+}
